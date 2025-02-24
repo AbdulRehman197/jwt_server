c45ae8cb1ed1b078ac8b1637eafc3d5c6e9fbca3
AbdulRehman197 <abdulrehman19761@gmail.com>
2025-02-24 12:12:33 +0500

    update home route

--- server.js
+++ server.js
@@ -16,5 +16,9 @@ initializeDatabases();
 app.use('/api/auth', authRoutes);
 app.use('/api/files', fileRoutes);
 
+app.get('/', (req, res) => {
+    res.send('server is starting at port 5000!');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
